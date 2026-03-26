503a43c30a432bf4c07f90ed9f21098950f4766d
huimaohao <huimaohao@proton.me>
2026-03-26 13:34:54 +0800

    Add demo_aiohttp_lxml.py

---
+++ demo_aiohttp_lxml.py
@@ -0,0 +1,62 @@
+"""
+
+``` Shell
+pip install aiohttp
+pip install lxml
+
+# required by lxml
+pip install cssselect
+
+# type stubs for lxml
+pip install types-lxml
+```
+
+"""
+
+import asyncio
+import aiohttp
+import lxml.html
+
+
+async def demo_1():
+    async with aiohttp.ClientSession() as session:
+        base_url = "https://www.news.cn/"
+        async with session.get(base_url) as response:  # header is read here
+            assert response._body is None
+            html_text = await response.text()  # content is read here
+            assert response._body is not None
+
+    html_tree = lxml.html.fromstring(html_text)
+    href_list = html_tree.xpath("//a[@href]/@href")
+
+    with open("news_href_list.txt", "w") as file:
+        file.write(f"{len(href_list)}\n")
+        for href in href_list:
+            file.write(href + "\n")
+
+
+async def demo_2():
+    async with aiohttp.ClientSession() as session:
+        base_url = "https://www.gmw.cn/"
+        async with session.get(base_url) as response:  # header is read here
+            assert response._body is None
+            html_text = await response.text()  # content is read here
+            assert response._body is not None
+
+    html_tree = lxml.html.fromstring(html_text)
+    node_list = html_tree.cssselect("a[href]")
+    href_list = [node.get("href") for node in node_list]
+
+    with open("gmw_href_list.txt", "w") as file:
+        file.write(f"{len(href_list)}\n")
+        for href in href_list:
+            file.write(href + "\n")
+
+
+async def main():
+    await demo_1()
+    await demo_2()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
