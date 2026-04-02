4c3cee08e3da1ae4cda3050b2ce69f04c106395c
huimaohao <huimaohao@proton.me>
2026-04-02 22:55:10 +0800

    Add demo_scrapegraphai.py

---
+++ demo_scrapegraphai.py
@@ -0,0 +1,36 @@
+"""
+
+``` Shell
+pip install scrapegraphai
+
+# required by scrapegraphai
+playwright install
+```
+
+"""
+
+from scrapegraphai.graphs import SmartScraperGraph
+import json
+
+# Define the configuration for the scraping pipeline
+graph_config = {
+    "llm": {
+        "model": "ollama/llama3.2",
+        "temperature": 0,
+        "model_tokens": 8192,
+        "format": "json",
+    },
+    "verbose": True,
+    "headless": False,
+}
+
+# Create the SmartScraperGraph instance
+smart_scraper_graph = SmartScraperGraph(
+    prompt="Find information about the founders and what the company does",
+    source="https://scrapegraphai.com/",
+    config=graph_config,
+)
+
+# Run the pipeline
+result = smart_scraper_graph.run()
+print(json.dumps(result, indent=4))
