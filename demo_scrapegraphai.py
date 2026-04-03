279b090a73f92d01b76ea80f3d83eeede09ee565
huimaohao <huimaohao@proton.me>
2026-04-03 23:32:39 +0800

    Update demo_scrapegraphai.py

--- demo_scrapegraphai.py
+++ demo_scrapegraphai.py
@@ -12,25 +12,31 @@ playwright install
 from scrapegraphai.graphs import SmartScraperGraph
 import json
 
-# Define the configuration for the scraping pipeline
-graph_config = {
-    "llm": {
-        "model": "ollama/llama3.2",
-        "temperature": 0,
-        "model_tokens": 8192,
-        "format": "json",
-    },
-    "verbose": True,
-    "headless": False,
-}
 
-# Create the SmartScraperGraph instance
-smart_scraper_graph = SmartScraperGraph(
-    prompt="Find information about the founders and what the company does",
-    source="https://scrapegraphai.com/",
-    config=graph_config,
-)
+def demo_1():
+    # Define the configuration for the scraping pipeline
+    graph_config = {
+        "llm": {
+            "model": "ollama/llama3.2",
+            "temperature": 0,
+            "model_tokens": 8192,
+            "format": "json",
+        },
+        "verbose": True,
+        "headless": False,
+    }
 
-# Run the pipeline
-result = smart_scraper_graph.run()
-print(json.dumps(result, indent=4))
+    # Create the SmartScraperGraph instance
+    smart_scraper_graph = SmartScraperGraph(
+        prompt="Find information about the founders and what the company does",
+        source="https://scrapegraphai.com/",
+        config=graph_config,
+    )
+
+    # Run the pipeline
+    result = smart_scraper_graph.run()
+    print(json.dumps(result, indent=4))
+
+
+if __name__ == "__main__":
+    demo_1()
